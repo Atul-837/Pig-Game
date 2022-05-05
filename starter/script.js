565df34c650f4600d050ed563b2d190586580861
Atul-837 <atul23547@gmail.com>
2022-05-05 03:22:41 -0400

    Final Game

--- starter/script.js
+++ starter/script.js
@@ -4,19 +4,20 @@ const player0 = document.querySelector('.player--0');
 const player1 = document.querySelector('.player--1');
 const score0 = document.querySelector('#score--0');
 const score1 = document.querySelector('#score--1');
-const currentScore0 = document.querySelector('#current--0');
-const currentScore1 = document.querySelector('#current--1');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 const btnNew = document.querySelector('.btn--new');
-const img = document.querySelector('.dice');
+const diceEl = document.querySelector('.dice');
+const modal = document.querySelector('.modal');
+const winner = document.querySelector('.modal-content');
 ///////////////////////////////////////////////////////////////
 score0.textContent = 0;
 score1.textContent = 0;
-const scores = [0, 0];
+let scores = [0, 0];
 let points = 0;
 let activePlayer = 0;
-img.classList.add('hidden');
+diceEl.classList.add('hidden');
+
 ////////////////////////////////////////////////////////////////
 
 const number = function () {
@@ -28,24 +29,53 @@ const currentPoints = function (ap, points) {
 const displayScore = function (ap, thatScore) {
   document.querySelector(`#score--${ap}`).textContent = thatScore;
 };
-
+const changePlayer = function (activePlayer) {
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.add('player--active');
+  let inactive = activePlayer === 1 ? 0 : 1;
+  document
+    .querySelector(`.player--${inactive}`)
+    .classList.remove('player--active');
+};
+const displayWinner = function (activePlayer) {
+  modal.style.display = 'block';
+  winner.textContent = `Player ${activePlayer + 1} is the WINNER!!!!!`;
+};
 btnRoll.addEventListener('click', function () {
   let diceRoll = number();
-  img.classList.remove('hidden');
-  img.src = img.src = `dice-${diceRoll}.png`;
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${diceRoll}.png`;
   if (diceRoll !== 1) {
     points = diceRoll + points;
     currentPoints(activePlayer, points);
   } else {
     currentPoints(activePlayer, 0);
     activePlayer = activePlayer === 0 ? 1 : 0;
+    changePlayer(activePlayer);
     points = 0;
   }
 });
 btnHold.addEventListener('click', function () {
-  scores[activePlayer] = scores[activePlayer] + points;
-  displayScore(activePlayer, scores[activePlayer]);
+  if (scores[activePlayer] < 100) {
+    scores[activePlayer] = scores[activePlayer] + points;
+    displayScore(activePlayer, scores[activePlayer]);
+    points = 0;
+    currentPoints(activePlayer, points);
+    if (scores[activePlayer] >= 100) displayWinner(activePlayer);
+    else {
+      activePlayer = activePlayer === 0 ? 1 : 0;
+      changePlayer(activePlayer);
+    }
+  }
+});
+
+btnNew.addEventListener('click', function () {
+  diceEl.classList.add('hidden');
   points = 0;
-  currentPoints(activePlayer, points);
-  activePlayer = activePlayer === 0 ? 1 : 0;
+  activePlayer = 0;
+  scores = [0, 0];
+  score0.textContent = 0;
+  score1.textContent = 0;
+  modal.style.display = 'none';
 });
